3942a4c02f1a290d7b5416457d14495cd801a5c7
MohammadRezaQaderi <mrq112775@outlook.com>
2020-09-16 14:00:55 +0430

    the image adding

--- Learning-Project/resources/views/welcome.blade.php
+++ Learning-Project/resources/views/welcome.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.layout')
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-<!--  @if (Route::has('login'))
+    <div >
+    <img src="/img/a1.jpg" alt="baby" style="width: 360px; height: 580px;">
+    </div>
+
+    <div class="titel" style="position:relative; top: 300px ; right: 340px;">
+        The Beautiful and Hot babe
+    </div>
+    <!--  @if (Route::has('login'))
     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
         @auth
             <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
@@ -105,4 +112,4 @@
             Build v{{ Illuminate\Foundation\Application::VERSION }}
         </div> -->
 </div>
-@endsction
+@endsection
